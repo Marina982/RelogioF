0a2f73fd7f87f81fb3f4c8177d8f82739f6bd427
Marina982 <marina.goes@estudante.ifms.edu.br>
2025-10-16 12:41:28 -0300

    ajuste css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
 import styles from './Header.module.css';
 
 const Header = () => {
   return (
-    <header className={styles.header}>
-      <h1 className={styles.title}>Formatura Informática 6A 🎓</h1>
-      <p className={styles.subtitle}>Marina Raquel Oliveira Maciel Oliveira Maciel Goes</p>
-      <p className={styles.date}>15 de Dezembro às 19:00h</p>
-    </header>
+    <>
+      <header className={styles.header}>
+        <h1 className={styles.title}>Formatura 2025 🎓</h1>
+        <p className={styles.subtitle}>Turma de Informática 6A</p>
+        <p className={styles.date}>15 de Dezembro de 2025 - 19h</p>
+      </header>
+      <div className={styles.spacer}></div>
+    </>
   );
 };
 
